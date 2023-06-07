fd64b159c4ff6b7ab310c27eca384f031e2b5939
Soba <sosobangee@gmail.com>
2023-06-07 14:44:15 +0800

    hauwhauwhauhelp
    updated story

--- Assets/Scripts/TextGame_Manager.cs
+++ Assets/Scripts/TextGame_Manager.cs
@@ -3,56 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class TextGame_Manager : MonoBehaviour
-{
+public class TextGame_Manager : MonoBehaviour {
     // variable int
-    public int hunger;
-    public int sanity;
+    public int hunger, sanity;
+
     // variable textmesh pro
-    public TextMeshProUGUI hungerTextValue, 
-                           sanityTextValue,
-                           storyTextValue;
+    public TextMeshProUGUI hungerTextValue, sanityTextValue, storyTextValue;
+
     // variable string
     public string start;
+
     // declare game object
-    public GameObject level1_Choices;
+    public GameObject hungerText, sanityText, level1_Choices, stats, mainScreen, choices;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        hunger = 10;
+    void Start() {
+        hunger = 0;
         sanity = 100;
         start = "can you see me?";
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         // textmeshpro.variable = value int
         hungerTextValue.text = hunger.ToString();
         sanityTextValue.text = sanity.ToString();
         storyTextValue.text = start;
     }
 
-    public void Yes()
-    {
+    // buttons
+    public void StartButton() {
+        choices.SetActive(true);
+        hungerText.SetActive(true);
+        sanityText.SetActive(true);
+        mainScreen.SetActive(false);
+    }
+    public void ExitButton() {
+        Application.Quit();
+    }
+
+
+    public void Yes() {
         sanity -= 10;
         hunger += 5;
         start = "Good";
         level1_Choices.SetActive(false);
     }
-    public void No()
-    {
+    public void No() {
         sanity -= 10;
         hunger += 80;
         start = "Oh ?";
         level1_Choices.SetActive(false);
     }
-    public void DontAnswer()
-    {
+    public void DontAnswer() {
         sanity -= 80;
         hunger += 40;
         start = "How boring";
         level1_Choices.SetActive(false);
     }
 }
+
